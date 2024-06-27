f78b5a2927cf788fd10fe09228dd8a56b6f4b417
Ramiro Gómez <code@ramiro.org>
2024-06-27 02:00:20 +0200

    Use name from about

--- charla/config.py
+++ charla/config.py
@@ -1,13 +1,17 @@
 from platformdirs import user_cache_dir, user_documents_dir
 
+from charla import __about__
+
+
+name = __about__.__name__
 
 default_settings = {
     'model': '',
-    'chats_path': user_documents_dir() + '/charla/chats',
-    'prompt_history': user_cache_dir(appname='charla') + '/prompt-history.txt'
+    'chats_path': user_documents_dir() + f'/{name}/chats',
+    'prompt_history': user_cache_dir(appname=name) + '/prompt-history.txt'
 }
 
 
-def setting(name: str, value: str = None) -> str:
-    return value if value else default_settings.get(name)
+def setting(key: str, value: str = None) -> str:
+    return value if value else default_settings.get(key)
 
