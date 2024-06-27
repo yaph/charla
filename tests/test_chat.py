16d8664682a055c4ed86ee3c3a0357728b189937
Ramiro Gómez <code@ramiro.org>
2024-06-27 02:10:21 +0200

    Fix test

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -1,12 +1,12 @@
 from charla import chat
 
 
-def test_chat_available_models():
+def test_available_models():
     models = chat.available_models()
     assert len(models)
     assert 'name' in models[0]
 
 
 def test_prompt_session():
-    session = chat.prompt_session()
+    session = chat.prompt_session('cache/prompt-history.txt')
     assert session.message == chat.t_prompt
