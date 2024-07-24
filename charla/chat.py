428fd7791b77f96c8863931582952157bc411e82
Ramiro Gómez <code@ramiro.org>
2024-07-24 23:48:31 +0200

    Add info to help.

--- charla/chat.py
+++ charla/chat.py
@@ -29,7 +29,7 @@ Press CTRL-C or CTRL-D to exit chat.
 Press RETURN to send prompt in single line mode.
 Press ALT+M to switch between single and multi line mode.
 Press ALT+RETURN to send prompt in multi line mode.
-Press CTRL-O to open file and append its content to the prompt.
+Press CTRL-O to open a file or web page and append its content to the prompt.
 Press CTRL-R or CTRL-S to search prompt history.
 Press ↑ and ↓ to navigate previously entered prompts.
 Press → to complete an auto suggested prompt.
